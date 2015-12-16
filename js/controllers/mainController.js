e188d1f89ae6addd85cf40436620d14f8d62554e
Frankie <frankievx@users.noreply.github.com>
2015-12-16 11:23:06 -0500

    Added a dice roll function

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,5 +1,11 @@
 app.controller("mainController", ["$scope",
 function($scope)
 {
-                                      
+  $scope.title = "Let's Settle This";
+  $scope.roll = function() {
+    var randomRoll = function() {
+      1 + Math.floor(Math.rand() * 6);
+    };
+    return randomRoll + randomRoll;
+  }
 }])
