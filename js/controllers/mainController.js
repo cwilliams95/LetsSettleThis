0bd6340bc19ebb0e1e41814b447c212f2bfa4016
Frankie <frankievx@users.noreply.github.com>
2015-12-16 12:28:53 -0500

    Added players and counters for resources

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,11 +1,22 @@
 app.controller("mainController", ["$scope",
-function($scope)
-{
+function($scope)  {
   $scope.title = "Let's Settle This";
   $scope.roll = function() {
     var randomRoll = function() {
       1 + Math.floor(Math.rand() * 6);
     };
     return randomRoll + randomRoll;
-  }
-}])
+  };
+  $scope.player1 = {
+    var lumber, grain, brick, wool, ore, victoryPoints = 0;
+  };
+  $scope.player2 = {
+    var lumber, grain, brick, wool, ore, victoryPoints = 0;
+  };
+  $scope.player3 = {
+    var lumber, grain, brick, wool, ore, victoryPoints = 0;
+  };
+  $scope.player4 = {
+    var lumber, grain, brick, wool, ore, victoryPoints = 0;
+  };
+}]);
