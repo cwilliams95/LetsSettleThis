29e99e7a5e654d75a830f14bb8ae01da246e3733
Frankie <frankievx@users.noreply.github.com>
2015-12-18 10:36:55 -0500

    Update mainController.js

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,24 +1,29 @@
 app.controller("mainController", ["$scope",
 function($scope)  {
   $scope.title = "Let's Settle This";
+  
+  //  Random Dice Roll function
   $scope.roll = function() {
     var randomRoll = function() {
       return 1 + Math.floor(Math.rand() * 6);
     };
     return randomRoll + randomRoll;
   };
-  $scope.players = function(name) {
+  $scope.newplayer = function (name) {
     this.name = name;
     this.points = 0;
     this.resources = {
-      "lumber": null,
-      "grain": null,
-      "wool": null,
-      "ore": null,
-      "brick": null,
+      "lumber": [{ "value": null, "multiplier": 1, "count": 0}],
+      "grain": [{ "value": null, "multiplier": 1, "count": 0}],
+      "wool": [{ "value": null, "multiplier": 1, "count": 0}],
+      "ore": [{ "value": null, "multiplier": 1, "count": 0}],
+      "brick": [{ "value": null, "multiplier": 1, "count": 0}]
     };
-    this.tiles = function()
-  }
+  };
+  $scope.newSettlement = function(name, tiles) {
+      if(tiles.lumber === true) {
+        players.name.resources 
+    }
   $scope.library = function(player) {
     return this.points += this.points
   $scope.monopoly = function(player, resource) {
